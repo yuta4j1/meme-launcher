dbf38f8aeac52133d0fc81a8917f78c5be507af8
yuta4j1 <kascado.ys10@gmail.com>
2024-01-03 23:30:56 +0900

    delete: unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import Header from "./components/Header";
 import ImageList from "./components/ImageList";
