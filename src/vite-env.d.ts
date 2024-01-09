efcb456a19750fad6b145c2abe0386a5beac5b0b
yuta4j1 <kascado.ys10@gmail.com>
2024-01-09 23:43:42 +0900

    add: environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_R2_ACCOUNT_ID: string;
+  readonly VITE_ACCESS_KEY_ID: string;
+  readonly VITE_SECRET_ACCESS_KEY: string;
+}
