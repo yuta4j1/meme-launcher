12bcd1210750f164c053700068d0d06905c2e302
yuta4j1 <kascado.ys10@gmail.com>
2024-01-11 22:24:49 +0900

    add: post request

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,3 +9,14 @@ export const getRequest = async <T>(path: string): Promise<T> => {
   });
   return res.json();
 };
+
+export const postRequest = async <P, R>(path: string, param: P): Promise<R> => {
+  const res = await fetch(API_BASE_URL + path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      body: JSON.stringify(param),
+    },
+  });
+  return res.json();
+};
