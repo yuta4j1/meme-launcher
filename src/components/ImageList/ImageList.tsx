21a50337ec4c3f3307afc2fa683bd2a7f74b3663
yuta4j1 <kascado.ys10@gmail.com>
2024-01-14 17:37:33 +0900

    add: image preview function

--- src/components/ImageList/ImageList.tsx
+++ src/components/ImageList/ImageList.tsx
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import useSWR from "swr";
 import { getRequest } from "../../api";
 import type { ImageListReponse } from "../../types/image";
+import PreviewModal from "../PreviewModal";
 import "./ImageList.css";
 
-function Image(props: { url: string; altText: string }) {
-  const { url, altText } = props;
-  return <img src={url} width={270} height={180} alt={altText} />;
+function ClickableImage(props: {
+  id: string;
+  url: string;
+  altText: string;
+  handleClick: (imageId: string) => void;
+}) {
+  const { id, url, altText, handleClick } = props;
+  return (
+    <button
+      className="preview-button"
+      aria-label="画像プレビューボタン"
+      onClick={() => handleClick(id)}
+    >
+      <img src={url} width={270} height={180} alt={altText} />
+    </button>
+  );
 }
 
 export function ImageList() {
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
+  const [previewedImageId, setPreviewImageId] = useState("");
   const {
     data: imageList,
     isLoading,
     error,
   } = useSWR<ImageListReponse>("/images", getRequest);
+
+  const handleImageClick = (imageId: string) => {
+    setPreviewImageId(imageId);
+    setIsPreviewOpen(true);
+  };
   if (isLoading) {
     return <div>loading...</div>;
   }
@@ -21,11 +43,28 @@ export function ImageList() {
     return <div>error: {error}</div>;
   }
   return (
-    <div className="container">
-      {imageList &&
-        imageList.map((image) => (
-          <Image key={image.id} url={image.imageUrl} altText={"test"} />
-        ))}
+    <div>
+      {imageList && (
+        <div className="images-container">
+          {imageList.map((image) => (
+            <ClickableImage
+              key={image.id}
+              id={String(image.id)}
+              url={image.imageUrl}
+              handleClick={handleImageClick}
+              altText={""}
+            />
+          ))}
+          <PreviewModal
+            open={isPreviewOpen}
+            onClose={() => setIsPreviewOpen(false)}
+            previewImage={
+              imageList.find((img) => String(img.id) === previewedImageId) ??
+              imageList[0]
+            }
+          />
+        </div>
+      )}
     </div>
   );
 }
