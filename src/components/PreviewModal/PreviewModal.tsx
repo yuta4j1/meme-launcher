0848cd41f02f8a43d78f0561009d427a970effd8
yuta4j1 <kascado.ys10@gmail.com>
2024-01-14 17:35:58 +0900

    add: Preview Modal

---
+++ src/components/PreviewModal/PreviewModal.tsx
@@ -0,0 +1,40 @@
+import { type FC } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import "./PreviewModal.css";
+
+type PreviewImage = {
+  id: number;
+  imageUrl: string;
+  categoryId: number;
+  tagList: string[];
+};
+
+export const PreviewModal: FC<{
+  open: boolean;
+  onClose: () => void;
+  previewImage: PreviewImage;
+}> = ({ open, onClose, previewImage }) => {
+  return (
+    <Dialog.Root
+      open={open}
+      onOpenChange={() => {
+        onClose();
+      }}
+    >
+      <Dialog.Portal>
+        <Dialog.Overlay className="preview-modal-overlay" />
+        <Dialog.Content
+          className="preview-modal-content"
+          style={{
+            backgroundImage: `url(${previewImage.imageUrl})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+          }}
+        >
+          {/* <img src={previewImage.imageUrl} alt="" className="preview-image" /> */}
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
