eba8200caa2745fc19a652fb87ffdb1961fa0aae
yuta4j1 <kascado.ys10@gmail.com>
2024-03-09 23:40:44 +0900

    setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+async function enableMocking() {
+  if (import.meta.env.MODE !== "mock") {
+    return;
+  }
+  const { worker } = await import("./mocks/browser");
+  return worker.start();
+}
+
+enableMocking().then(() =>
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
