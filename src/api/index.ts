7df4062e7fb5a71bc48a83c460ea212b0f942075
yuta4j1 <kascado.ys10@gmail.com>
2024-03-09 23:41:00 +0900

    export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://meme-launcher-server.kascado-ys10.workers.dev";
+export const API_BASE_URL =
+  "https://meme-launcher-server.kascado-ys10.workers.dev";
 
 export const getRequest = async <T>(path: string): Promise<T> => {
   const res = await fetch(API_BASE_URL + path, {
