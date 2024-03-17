9d02d13de9dc1483884454206395c021024cd15f
yuta4j1 <kascado.ys10@gmail.com>
2024-03-17 14:25:05 +0900

    delete: unused file

--- pscale_dump_meme_launcher_main_20240309_120654/meme_launcher.category-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `category` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `emoji` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- pscale_dump_meme_launcher_main_20240309_120654/meme_launcher.meme_tag-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `meme_tag` (
-  `meme_id` int NOT NULL,
-  `tag_id` int NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`meme_id`,`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
